0df6628a5167f362449da72c318dfd74d59be8f0
Nico Pareigis <nico@pareigis.de>
2022-06-25 12:59:36 +0200

    Add custom rofi launch script.

---
+++ source/.config/rofi/rofi_custom.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+Lines=''
+while IFS='\n' read -r Line; do
+  Lines="$Lines$Line\n"
+done < ~/.config/rofi/program_list
+
+Program=$(printf $Lines | rofi -monitor -4 -disable-history -dmenu)
+$Program &
