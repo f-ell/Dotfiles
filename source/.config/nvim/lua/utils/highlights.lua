f1cf4be1d6b39304a56ffd047db299d678c1ea93
Nico Pareigis <nico@pareigis.de>
2022-08-17 00:16:34 +0200

    add: some new highlight groups

--- source/.config/nvim/lua/utils/highlights.lua
+++ source/.config/nvim/lua/utils/highlights.lua
@@ -1,17 +1,25 @@
 local set_hl_groups = function()
   local fg_1 = '#ffffff'
   local fg_2 = '#b0b0b0'
+
+  local dark_blue = '#400070'
+  local light_blue = '#8020f0'
+
   local hls = {
     -- {0, 'name', {fg = '', bg = ''}},
 
     {0, 'bg',     {fg = fg_1, bg = ''}},
     {0, 'ro',     {fg = fg_1, bg = '#ff3050'}},
 
-    {0, 'Search',       {fg = fg_1, bg = '#8020f0'}},
-    {0, 'Visual',       {fg = '',   bg = '#500040'}},
+    {0, 'Search',     {fg = dark_blue,  bg = fg_1}},
+    {0, 'Substitute', {fg = dark_blue,  bg = fg_1}},
+    {0, 'CurSearch',  {fg = fg_1,       bg = dark_blue}}, -- doesn't work?
+    {0, 'IncSearch',  {fg = fg_1,       bg = dark_blue}},
 
-    {0, 'TabLine',      {fg = fg_2, bg = '#400070'}},
-    {0, 'TabLineSel',   {fg = fg_1, bg = '#8020f0'}},
+    {0, 'Visual', {fg = '', bg = '#500040'}},
+
+    {0, 'TabLine',      {fg = fg_2, bg = dark_blue}},
+    {0, 'TabLineSel',   {fg = fg_1, bg = light_blue}},
     {0, 'TabLineFill',  {fg = fg_1, bg = ''}},
 
     {0, 'modeN',  {fg = fg_1, bg = '#8020f0'}},
