0618a5e0b9c21de8f7f92612b5d0640dad6f093c
Nico Pareigis <nico@pareigis.de>
2022-08-17 18:21:45 +0200

    add: refactor and remove markdown mappings in favour of snippets

--- source/.config/nvim/lua/utils/keymaps.lua
+++ source/.config/nvim/lua/utils/keymaps.lua
@@ -38,96 +38,85 @@ end
 
 vim.g.mapleader = ' '
 
--- QOL
--- n
+-- normal
 F.nnmap('<leader>sk', ':source ~/.config/nvim/lua/utils/keymaps.lua<CR>')
 
 F.nnmap('--', ':w<CR>')
-F.nnmap('<leader>bw', ':bw<CR>')
+F.nnmap('-w', ':bw!<CR>')
 F.nnmap('<C-l>', ':noh<CR>')
+F.nnmap('<leader><CR>', ':lua term()<CR>')
 
+F.nnmap('<leader>~', 'viw~')
 F.nnmap('<leader>rcl', '"*y_:lua <C-r>*<CR>')
 
 
-F.nnmap('<leader>~', 'viw~')
-F.nnmap('<leader><CR>', ':lua term()<CR>')
-
 F.nnmap('<A-f>', ':FZF -i --reverse --scroll-off=1 --no-info --no-color --prompt=$ ~<CR>')
 
--- v
-F.vnmap('<Tab>', '>gv')
-F.vnmap('<S-Tab>', '<gv')
+-- visual
+F.vnmap('<Tab>',    '>gv')
+F.vnmap('<S-Tab>',  '<gv')
 
--- c
+-- command
 F.cnmap('<C-h>', '<Left>')
 F.cnmap('<C-k>', '<Up>')
 F.cnmap('<C-j>', '<Down>')
 F.cnmap('<C-l>', '<Right>')
 
--- t
+-- terminal
 F.tnmap('<C-d>', '<C-\\><C-n>')
 
 
-
--- PACKER
-F.nnmap('<leader>ps', ':PackerStatus<CR>')
-F.nnmap('<leader>pc', ':PackerCompile<CR>')
-F.nnmap('<leader>py', ':PackerSync<CR>')
-
-
--- TABS
-F.nnmap('<leader>h', ':tabnext-<CR>')
-F.nnmap('<leader>j', ':tabfirst<CR>')
-F.nnmap('<leader>l', ':tabnext+<CR>')
-F.nnmap('<leader>k', ':tablast<CR>')
-
-F.nnmap('<leader><C-h>', ':tabmove-<CR>')
-F.nnmap('<leader><C-j>', ':tabmove0<CR>')
-F.nnmap('<leader><C-l>', ':tabmove+<CR>')
-F.nnmap('<leader><C-k>', ':tabmove$<CR>')
+-- html / css / js
+F.nnmap('<leader>br', 'oborder: 1px solid red;<Esc>o<Esc>')
+F.nnmap('<leader>bg', 'oborder: 1px solid green;<Esc>o<Esc>')
+F.nnmap('<leader>bb', 'oborder: 1px solid blue;<Esc>o<Esc>')
 
 
--- SPLITS
-F.nmap('<A-CR>', ':vnew<CR><A-f>')
-F.nmap('<leader><A-CR>', ':new<CR><A-f>')
+-- tabs
+F.nnmap('<leader>th', ':tabnext-<CR>')
+F.nnmap('<leader>tj', ':tabfirst<CR>')
+F.nnmap('<leader>tl', ':tabnext+<CR>')
+F.nnmap('<leader>tk', ':tablast<CR>')
+
+F.nnmap('<leader>t<S-h>', ':tabmove-<CR>')
+F.nnmap('<leader>t<S-j>', ':tabmove0<CR>')
+F.nnmap('<leader>t<S-l>', ':tabmove+<CR>')
+F.nnmap('<leader>t<S-k>', ':tabmove$<CR>')
+
+
+-- splits
+F.nmap('<A-CR>',          ':vnew<CR><A-f>')
+F.nmap('<leader><A-CR>',  ':new<CR><A-f>')
 
 F.nnmap('<A-h>', '<C-w>h')
 F.nnmap('<A-j>', '<C-w>j')
 F.nnmap('<A-k>', '<C-w>k')
 F.nnmap('<A-l>', '<C-w>l')
 
--- map('n', '<C-h>', ':4winc <<CR>')
--- map('n', '<C-j>', ':4winc +<CR>')
--- map('n', '<C-k>', ':4winc -<CR>')
--- map('n', '<C-l>', ':4winc ><CR>')
-
--- F.map('n', '<A-.>', '<C-w>|') -- might need backslash-escape
--- F.map('n', '<A-->', '<C-w>_')
+-- F.nnmap('<C-h>', ':4winc <<CR>')
+-- F.nnmap('<C-j>', ':4winc +<CR>')
+-- F.nnmap('<C-k>', ':4winc -<CR>')
+-- F.nnmap('<C-l>', ':4winc ><CR>')
 
 
--- HTML / CSS / JS
-F.nnmap('<leader>br', 'oborder: 1px solid red;<Esc>o<Esc>')
-F.nnmap('<leader>bg', 'oborder: 1px solid green;<Esc>o<Esc>')
-F.nnmap('<leader>bb', 'oborder: 1px solid blue;<Esc>o<Esc>')
+-- plugins
+  -- packer
+    F.nnmap('<leader>ps', ':PackerStatus<CR>')
+    F.nnmap('<leader>pc', ':PackerCompile<CR>')
+    F.nnmap('<leader>py', ':PackerSync<CR>')
 
+  -- colorizer
+    F.nnmap('<leader>ct', ':ColorizerToggle<CR>')
 
--- Markdown
-F.nnmap('<leader>"', 'a``<Esc>')
-F.nnmap('<leader>\'', 'o```<CR>```<Esc>kA')
+  -- trouble
+    F.nnmap('<leader>tt', ':TroubleToggle<CR>')
 
+  -- nvim-tree
+    F.nnmap('<leader>ntt', ':NvimTreeToggle<CR>')
 
--- Plugins
-  -- Colorizer
-    F.nnmap('<leader>c', ':ColorizerToggle<CR>')
-  -- Trouble
-    F.nnmap('<leader>t', ':TroubleToggle<CR>')
-
-  -- Nvim-Tree
-    F.nnmap('<leader>e', ':NvimTreeToggle<CR>')
-
-  -- Emmet
+  -- emmet
     F.inmap('<A-e>', '<C-y>,<Esc>')
     F.nnmap('<A-e>', '<C-y>,<Esc>')
 
-  -- MarkdownPreview
+  -- markdownpreview
     F.nnmap('<A-p>', '<plug>MarkdownPreviewToggle')
