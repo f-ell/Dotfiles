cb090fffaa8c8564cffa8633c1d6d607c54a83b3
Nico Pareigis <nico@pareigis.de>
2022-08-24 23:05:09 +0200

    change: consistent single quotes

--- source/.config/nvim/lua/lsp/init.lua
+++ source/.config/nvim/lua/lsp/init.lua
@@ -8,14 +8,14 @@ require('lsp.server-setup')
 
 local base_setup = function()
   local signs = {
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn",  text = "" },
-    { name = "DiagnosticSignHint",  text = "" },
-    { name = "DiagnosticSignInfo",  text = "" },
+    { name = 'DiagnosticSignError', text = '' },
+    { name = 'DiagnosticSignWarn',  text = '' },
+    { name = 'DiagnosticSignHint',  text = '' },
+    { name = 'DiagnosticSignInfo',  text = '' },
   }
   for _, sign in pairs(signs) do
     vim.fn.sign_define(sign.name,
-      { texthl = sign.name, text = sign.text, numhl = "" })
+      { texthl = sign.name, text = sign.text, numhl = '' })
   end
 
   local config = {
