8ea660d281f4262c7dd6d9c79adcf9d52455879c
Nico Pareigis <nico@pareigis.de>
2022-09-11 15:00:15 +0200

    change: add other programs and refactor script

--- source/.config/rofi/rofi_custom.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-Lines=''
-while IFS='\n' read -r Line; do
-  Lines="$Lines$Line\n"
-done < ~/.config/rofi/program_list
-
-Program=$(printf $Lines | rofi -monitor -4 -disable-history -dmenu -i)
-$Program &
