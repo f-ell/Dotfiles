1df7a3853c7e2ad31af560b2675514361dd6a748
Nico Pareigis <nico@pareigis.de>
2022-11-05 22:57:48 +0100

    refactor: improve formatting

--- source/.config/browser/js.js
+++ source/.config/browser/js.js
@@ -1,22 +1,26 @@
 const dayElem = document.getElementsByClassName('date')[0];
 const time    = document.getElementsByClassName('time')[0];
 
-const weekdays  = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-const timeFunc = function() {
-  const date    = new Date();
+const pad = function(num, len, sym) {
+  return num.toString().padStart(len, sym);
+}
 
-  let day   = date.getDate();   if (day < 10)   day   = `0${day}`;
-  let month = date.getMonth();  if (month < 10) month = `0${month}`;
+const getTime = function() {
+  const date  = new Date();
+
+  let day     = pad(date.getDate(), 2, '0');
+  let month   = pad(date.getMonth(), 2, '0');
   const year  = date.getFullYear();
 
   const weekday = weekdays[date.getDay()];
   const hour    = date.getHours();
-  let minute    = date.getMinutes(); if (minute < 10) minute = `0${minute}`;
+  let minute    = pad(date.getMinutes(), 2, '0');
 
-  let ampm = 'PM'; if (hour < 12) ampm = 'AM';
+  hour < 12 ? ampm = 'AM' : ampm = 'PM';
 
   dayElem.textContent = `${weekday}. ${day}.${month}.${year}`;
   time.textContent = `${hour%12 ? hour%12 : 12}:${minute} ${ampm}`;
 }
-setInterval(timeFunc, 1000);
+setInterval(getTime, 1000);
