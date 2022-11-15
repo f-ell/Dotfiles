3a61f0a73730dc9ad365b54e2482a097a65735bf
Nico Pareigis <nico@pareigis.de>
2022-11-15 17:23:35 +0100

    chore: change code_action icon

--- source/.config/nvim/lua/plugins/lspsaga-nvim.lua
+++ source/.config/nvim/lua/plugins/lspsaga-nvim.lua
@@ -9,7 +9,7 @@ require('lspsaga').init_lsp_saga({
     sign              = false,
     update_time       = 100
   },
-  code_action_icon  = '',
+  code_action_icon  = '',
   max_preview_lines = 8,
 
   rename_action_quit  = '<C-c>',
