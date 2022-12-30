cded434249f7b630ec030dc25d6fb12e94dccbb1
Nico Pareigis <nico@pareigis.de>
2022-12-30 23:07:47 +0100

    chore: migrate to everforest-ish ranger theme

--- source/.config/ranger/colorschemes/everforest.py
+++ source/.config/ranger/colorschemes/everforest.py
@@ -10,8 +10,8 @@ from ranger.gui.color import default_colors, reverse, bold, normal, default
 
 
 # pylint: disable=too-many-branches,too-many-statements
-class Zenburn(ColorScheme):
-    progress_bar_color = 108
+class everforest(ColorScheme):
+    progress_bar_color = 8
 
     def use(self, context):
         fg, bg, attr = default_colors
@@ -25,93 +25,89 @@ class Zenburn(ColorScheme):
             else:
                 attr = normal
             if context.empty or context.error:
-                fg = 174
-                bg = 235
+                fg = 1
+                bg = 17
             if context.border:
-                fg = 7
+                fg = 9
             if context.image:
-                fg = 42
+                fg = 7
             if context.video:
-                fg = 45
+                fg = 15
             if context.audio:
-                fg = 165
+                fg = 1
             if context.document:
-                fg = 226
+                fg = 9
             if context.container:
                 attr |= bold
-                fg = 174
+                fg = 13
             if context.directory:
                 attr |= bold
-                fg = 229
+                fg = 15
             elif context.executable and not \
                     any((context.media, context.container,
                          context.fifo, context.socket)):
                 attr |= bold
-                fg = 197
+                fg = 13
             if context.socket:
-                fg = 180
+                fg = 2
                 attr |= bold
             if context.fifo or context.device:
-                fg = 144
+                fg = 15
                 if context.device:
                     attr |= bold
             if context.link:
-                fg = 122 if context.good else 160
+                fg = 7 if context.good else 13
             if context.bad:
-                bg = 234
+                bg = 14
             if context.tag_marker and not context.selected:
                 attr |= bold
-                if fg in (174, 95):
-                    fg = 248
-                else:
-                    fg = 174
+                fg = 1
             if not context.selected and (context.cut or context.copied):
-                fg = 108
-                bg = 234
+                fg = 2
             if context.main_column:
                 if context.selected:
                     attr |= bold
                 if context.marked:
                     attr |= bold
-                    fg = 158
+                    fg = 10
             if context.badinfo:
                 if attr & reverse:
-                    bg = 95
+                    bg = 14
                 else:
-                    fg = 95
+                    fg = 14
 
         elif context.in_titlebar:
             attr |= bold
             if context.hostname:
-                fg = 174 if context.bad else 180
+                fg = 1
             elif context.directory:
-                fg = 230
+                fg = 1
             elif context.tab:
                 if context.good:
-                    bg = 180
+                    bg = 2
             elif context.link:
-                fg = 116
+                fg = 7
 
         elif context.in_statusbar:
             if context.permissions:
                 if context.good:
-                    fg = 158
+                    fg = 5
                 elif context.bad:
-                    fg = 160
+                    fg = 13
             if context.marked:
                 attr |= bold | reverse
-                fg = 223
+                fg = 11
             if context.message:
                 if context.bad:
                     attr |= bold
-                    fg = 174
+                    fg = 15
             if context.loaded:
                 bg = self.progress_bar_color
             if context.vcsinfo:
-                fg = 116
+                fg = 7
                 attr &= ~bold
             if context.vcscommit:
-                fg = 144
+                fg = 5
                 attr &= ~bold
 
         if context.text:
@@ -120,7 +116,7 @@ class Zenburn(ColorScheme):
 
         if context.in_taskview:
             if context.title:
-                fg = 116
+                fg = 1
 
             if context.selected:
                 attr |= reverse
@@ -134,29 +130,29 @@ class Zenburn(ColorScheme):
         if context.vcsfile and not context.selected:
             attr &= ~bold
             if context.vcsconflict:
-                fg = 95
+                fg = 13
             elif context.vcschanged:
-                fg = 174
+                fg = 7
             elif context.vcsunknown:
-                fg = 174
+                fg = 2
             elif context.vcsstaged:
-                fg = 108
+                fg = 9
             elif context.vcssync:
-                fg = 108
+                fg = 7
             elif context.vcsignored:
                 fg = default
 
         elif context.vcsremote and not context.selected:
             attr &= ~bold
             if context.vcssync:
-                fg = 108
+                fg = 7
             elif context.vcsbehind:
-                fg = 174
+                fg = 13
             elif context.vcsahead:
-                fg = 116
+                fg = 9
             elif context.vcsdiverged:
-                fg = 95
+                fg = 11
             elif context.vcsunknown:
-                fg = 174
+                fg = 2
 
         return fg, bg, attr
