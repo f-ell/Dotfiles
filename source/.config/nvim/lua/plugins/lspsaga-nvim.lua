79e4ef7e647107d00ed84a9f479f007ed789174b
Nico Pareigis <nico@pareigis.de>
2023-01-16 23:35:28 +0100

    feat: migrate lspsaga config to new setup function

--- source/.config/nvim/lua/plugins/lspsaga-nvim.lua
+++ source/.config/nvim/lua/plugins/lspsaga-nvim.lua
@@ -1,25 +1,43 @@
 return {
   'glepnir/lspsaga.nvim',
-  lazy    = true,
+  lazy    = true, -- required as dependency in lsp_bootstrap.lua
   config  = function()
-    require('lspsaga').init_lsp_saga({
-      border_style      = 'rounded',
-      saga_winblend     = 0,
-      move_in_saga      = { prev = '<C-k>', next = '<C-j>' },
-      diagnostic_header = { ' ', ' ', ' ', 'ﴞ ' },
-      code_action_lightbulb = {
+    require('lspsaga').setup({
+      ui = {
+        border    = 'rounded',
+        winblend  = 0,
+        code_action = ' ',
+        diagnostic  = ' '
+      },
+      symbol_in_winbar = { enable = false },
+      preview = {
+        lines_above = 0,
+        lines_below = 8,
+      },
+      scroll_preview = {
+        scroll_up   = '<C-k>',
+        scroll_down = '<C-j>'
+      },
+
+      lightbulb = {
         enable            = true,
         enable_in_insert  = false,
         sign              = true,
-        virtual_text      = false,
-        update_time       = 100
+        virtual_text      = false
       },
-      code_action_icon  = ' ',
-      max_preview_lines = 8,
+      code_action = { num_shortcut = true, quit = '<C-c>' },
 
-      rename_action_quit  = '<C-c>',
-      rename_in_select    = true,
-      code_action_num_shortcut = true,
+      definition = { quit = '<C-c>' },
+
+      diagnostic = {
+        show_code_action = false,
+        keys = { quit = '<C-c>' }
+      },
+
+      rename = {
+        in_select = true,
+        quit = '<C-c>'
+      }
     })
   end
 }
