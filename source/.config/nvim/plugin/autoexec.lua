5108683339c71e3da30e56eb5b34b499313eb3a1
Nico Pareigis <nico@pareigis.de>
2023-02-14 23:38:00 +0100

    feat: improve AutoExec flexibility and allow changing command / detaching

--- source/.config/nvim/plugin/autoexec.lua
+++ source/.config/nvim/plugin/autoexec.lua
@@ -1,66 +1,172 @@
-local attach_to_buffer = function(_in_bufnr, _out_bufnr, command)
-  local in_bufnr  = tonumber(_in_bufnr)
-  local out_bufnr = tonumber(_out_bufnr)
-  local in_file   = vim.fn.bufname(in_bufnr)
+local v   = vim
+local va  = v.api
+local vf  = v.fn
+local vu  = v.ui
 
-  vim.api.nvim_create_autocmd('BufWritePost', {
-    group     = vim.api.nvim_create_augroup('AutoExec', {clear = true}),
-    pattern   = in_file,
+local augroup = va.nvim_create_augroup('autoexec', { clear = false })
+
+local clean_tbl = {
+  ogname = nil,
+  ogwin = nil,
+  ogbuf = nil,
+  aebuf = nil,
+  aewin = nil,
+  dir = 0, -- 0 -> hor, 1 -> ver
+  cmd = nil,
+  id  = nil
+}
+local autoexec = {
+  _bufname  = 'AutoExec',
+  ogname    = nil,
+  ogwin = nil,
+  ogbuf = nil,
+  aebuf = nil,
+  aewin = nil,
+  dir = 0, -- 0 -> hor, 1 -> ver
+  cmd = nil,
+  id  = nil
+}
+
+
+
+
+local reset = function()
+  if va.nvim_win_is_valid(autoexec.aewin) then
+    va.nvim_win_close(autoexec.aewin, true)
+  end
+
+  for k, v in pairs(clean_tbl) do
+    autoexec[k] = v
+  end
+end
+
+
+local has_win = function()
+  for _, winnr in pairs(va.nvim_list_wins()) do
+    if autoexec.aebuf == va.nvim_win_get_buf(winnr) then return true end
+  end
+  return false
+end
+
+
+
+
+local make_buf = function()
+  autoexec.aebuf = va.nvim_create_buf(false, true)
+  if autoexec.aebuf == 0 then
+    return v.notify('autoexec: couldn\'t create buffer', 4)
+  end
+
+  va.nvim_buf_set_name(autoexec.aebuf, autoexec._bufname)
+end
+
+
+local make_split = function()
+  local cmd = autoexec.dir == 0 and 'sp' or 'vsp'
+  v.cmd(cmd..' | b'..autoexec.aebuf)
+  autoexec.aewin = va.nvim_get_current_win()
+  va.nvim_set_current_win(autoexec.ogwin)
+end
+
+
+local update_split = function()
+  vu.select({ 'hor (-)', 'ver (|)' }, { prompt = 'split type: ' }, function(_, i)
+    if i == nil then return print('aborting...') end
+    autoexec.dir = i - 1
+  end)
+end
+
+
+local update_cmd = function()
+  vu.input({ prompt = 'cmd: ',
+    default = autoexec.cmd == nil and vf.expand('%:p') or autoexec.cmd },
+    function(str)
+      if str == nil then return print('aborting...') end
+      if str == '' then autoexec.cmd = vf.expand('%:p') end
+      autoexec.cmd = str
+  end)
+end
+
+
+local register_cmd = function()
+  if autoexec.id then return end
+
+  autoexec.id = va.nvim_create_autocmd('BufWritePost', {
+    group   = augroup,
+    buffer  = autoexec.ogbuf,
+    desc    = 'Update buffer contents with <command> output on save.',
     callback  = function()
-      local append = function(_, data)
-        if data then
-          vim.api.nvim_buf_set_lines(out_bufnr, -1, -1, false, data)
-        end
+      local set_data = function(_, data)
+        if not data then return end
+        va.nvim_buf_set_option(autoexec.aebuf, 'modifiable', true)
+        va.nvim_buf_set_lines(autoexec.aebuf, -1, -1, true, data)
+        va.nvim_buf_set_option(autoexec.aebuf, 'modifiable', false)
       end
 
-      vim.api.nvim_buf_set_lines(out_bufnr, 0, -1, false,
-        {in_file..' output:'})
-      vim.api.nvim_buf_set_lines(out_bufnr, -1, -1, false, {''})
+      va.nvim_buf_set_option(autoexec.aebuf, 'modifiable', true)
+      va.nvim_buf_set_lines(autoexec.aebuf, 0, -1, false, {
+        autoexec.cmd..' output:', '' })
+      va.nvim_buf_set_option(autoexec.aebuf, 'modifiable', false)
 
-      vim.fn.jobstart(command, {
-        stdout_buffered = true, -- get data linewise
-        on_stdout = append,
-        on_stderr = append
+      vf.jobstart(autoexec.cmd, {
+        detach = false,
+        stdout_buffered = true,
+        on_stdout = set_data,
+        on_stderr = set_data
       })
     end
   })
 end
 
 
-
-vim.api.nvim_create_user_command('AutoExec', function()
-  local exists = false
-  print('Attaching to buffer...')
-
-  local in_bufname  = vim.fn.bufname()
-  local in_bufnr    = vim.fn.bufnr(in_bufname)
-  local in_winid    = vim.fn.bufwinid(in_bufnr)
-  local out_bufnr
-
-  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
-    if vim.fn.bufname(bufnr) == 'AutoExec' then
-      exists    = true
-      out_bufnr = bufnr
+local register_del = function()
+  va.nvim_create_autocmd({ 'BufUnload', 'BufDelete', 'BufWipeout' }, {
+    group   = augroup,
+    buffer  = autoexec.aebuf,
+    desc    = 'Remove auto-update and reset context on :bd | :bw | unload.',
+    callback = function()
+      reset()
     end
+  })
+end
+
+
+
+
+va.nvim_create_user_command('AutoExec', function()
+  autoexec.ogwin   = va.nvim_get_current_win()
+  autoexec.ogbuf   = va.nvim_get_current_buf()
+  autoexec.ogname  = va.nvim_buf_get_name(autoexec.ogbuf)
+
+  if autoexec.aebuf == autoexec.ogbuf then
+    return v.notify('autoexec: can\'t attach to AutoExec buffer', 2)
   end
+  if autoexec.aebuf then reset() end
 
-  if not exists then
-    out_bufnr = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_name(out_bufnr, 'AutoExec')
+  print('attaching to buffer...')
 
-    local split_dir = vim.fn.input('[h]orizontal | [V]ertical: ')
-          split_dir = split_dir:lower()
+  if not autoexec.aebuf then make_buf() end
 
-    local split_cmd = ' | b'..out_bufnr..' | exe win_gotoid('..in_winid..')'
-    if split_dir == 'h' then  split_cmd = 'new'..split_cmd
-    else                      split_cmd = 'vnew'..split_cmd end
-    vim.api.nvim_command(split_cmd)
-  end
+  update_split()
+  if not has_win() then make_split() end
 
-  local command = vim.fn.input('command: ')
-  if command == '' then command = './'..in_bufname end
+  update_cmd()
+  register_cmd()
+  register_del()
 
-  attach_to_buffer(in_bufnr, out_bufnr, command)
-  print(' \nAttached.')
-  vim.api.nvim_command('silent w')
-end, {})
+  print(' \nattached successfully.')
+  v.cmd('silent w')
+end, { desc = 'Execute <command> whenever current buffer is written.' })
+
+
+va.nvim_create_user_command('AutoExecCmd', function()
+  update_cmd()
+  v.cmd('silent w')
+end, { desc = 'Update <command> without changing anything else.' })
+
+
+va.nvim_create_user_command('AutoExecDetach', function()
+  va.nvim_del_autocmd(autoexec.id)
+  va.nvim_buf_delete(autoexec.aebuf, { force = true })
+  reset()
+end, { desc = 'Clean up AutoExec and wipe AutoExec buffer.' })
