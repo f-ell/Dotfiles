4d01120754b416112388bf9f2d241ceb1022707f
Nico Pareigis <nico@pareigis.de>
2023-02-17 14:20:21 +0100

    fix: disable regex highlighting

--- source/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ source/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -22,7 +22,7 @@ return {
 
       highlight = {
         enable  = true,
-        additional_vim_regex_highlighting = true
+        additional_vim_regex_highlighting = false
       },
 
       context_commentstring = {
