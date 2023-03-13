6fcfaf36d4f2e6f3188f364a6891f83f92de176f
Nico Pareigis <nico@pareigis.de>
2023-03-13 08:41:21 +0100

    feat: add capabilities and extendedClientCapabilities via nvim-jdtls

--- source/.config/nvim/lua/lsp/servers/java.lua
+++ source/.config/nvim/lua/lsp/servers/java.lua
@@ -1,3 +1,4 @@
+-- capabilities and extendedClientCapabilities via https://github.com/mfussenegger/nvim-jdtls
 local vf  = vim.fn
 local msn = vf.stdpath('data')..'/mason'
 local mpc = msn..'/packages'
@@ -17,6 +18,7 @@ return {
     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
     '-jar', vf.glob(mpc..'/jdtls/plugins/org.eclipse.equinox.launcher_*.jar'),
     '-configuration', mpc..'/jdtls/config_linux',
+    '-data', vf.stdpath('data')..'/jdtls-workspace/'..vf.fnamemodify(vf.getcwd(), ':p:t')
   },
 
   -- https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
@@ -26,5 +28,39 @@ return {
       vf.glob(mpc..'/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar'),
       vf.glob(mpc..'/java-test/extension/server/*.jar'),
       mpc..'/jdtls/lombok.jar'
+    },
+    gibberish = 'str',
+    extendedClientCapabilities = {
+      classFileContentsSupport              = true,
+      generateToStringPromptSupport         = true,
+      hashCodeEqualsPromptSupport           = true,
+      advancedExtractRefactoringSupport     = true,
+      advancedOrganizeImportsSupport        = true,
+      generateConstructorsPromptSupport     = true,
+      generateDelegateMethodsPromptSupport  = true,
+      moveRefactoringSupport                = true,
+      overrideMethodsPromptSupport          = true,
+      inferSelectionSupport = {
+        'extractMethod',
+        'extractVariable',
+        'extractConstant',
+        'extractVariableAllOccurrence'
+      }
     }
-  }}
+  },
+  capabilities = {
+    textDocument = {
+      codeAction = {
+        codeActionLiteralSupport = {
+          codeActionKind = {
+            valueSet = {
+              'source.generate.toString',
+              'source.generate.hashCodeEquals',
+              'source.organizeImports',
+            }
+          }
+        }
+      }
+    }
+  }
+}
