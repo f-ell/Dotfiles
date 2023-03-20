fa8932895dff23b045759173ed3300b4c8db8255
Nico Pareigis <nico@pareigis.de>
2023-03-20 19:45:40 +0100

    feat: add vim.lsp.buf.refernces mapping

--- source/.config/nvim/lua/lsp/setup.lua
+++ source/.config/nvim/lua/lsp/setup.lua
@@ -24,11 +24,12 @@ local configs  = {
 
 
 local on_attach = function()
-  key.nnmap('gd',         ui.def.peek,        { buffer = 0 })
-  key.nnmap('<leader>gd', ui.def.open,        { buffer = 0 })
-  key.nnmap('K',          v.lsp.buf.hover,    { buffer = 0 })
-  key.nnmap('<leader>ca', ui.cda.codeaction,  { buffer = 0 })
-  key.nnmap('<leader>rn', ui.ren.rename,      { buffer = 0 })
+  key.nnmap('gd',         ui.def.peek,          { buffer = 0 })
+  key.nnmap('<leader>gd', ui.def.open,          { buffer = 0 })
+  key.nnmap('K',          v.lsp.buf.hover,      { buffer = 0 })
+  key.nnmap('<leader>ca', ui.cda.codeaction,    { buffer = 0 })
+  key.nnmap('<leader>rf', v.lsp.buf.references, { buffer = 0 })
+  key.nnmap('<leader>rn', ui.ren.rename,        { buffer = 0 })
 
   key.nnmap('<leader>h', ui.dgn.get_line,   { buffer = 0 })
   key.nnmap('<leader>j', ui.dgn.goto_next,  { buffer = 0 })
