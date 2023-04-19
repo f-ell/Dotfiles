d5e302cd8f63b0e7cf159ae74910c9b3b956e08f
Nico Pareigis <nico@pareigis.de>
2023-04-19 21:27:09 +0200

    feat(lsp): fully decouple server setup and configuration

--- source/.config/nvim/lua/lsp/setup.lua
+++ source/.config/nvim/lua/lsp/setup.lua
@@ -3,20 +3,6 @@ local key = require('utils.lib').key
 local ui  = require('lsp.ui')
 local v   = vim
 
-local servers = {
-  'clangd',
-  'cssls',
-  'gopls',
-  'html',
-  'jdtls',
-  'lua_ls',
-  'perlpls',
-  'pylsp',
-  'rust_analyzer',
-  'texlab',
-  'tsserver'
-}
-
 local on_attach = function()
   key.nnmap('gd',         ui.def.peek,          { buffer = 0 })
   key.nnmap('<leader>gd', ui.def.open,          { buffer = 0 })
@@ -36,10 +22,11 @@ end
 local capabilities = require('cmp_nvim_lsp')
   .default_capabilities(v.lsp.protocol.make_client_capabilities())
 
-for i = 1, #servers do
+for _, server in pairs(v.fn.readdir(v.fn.stdpath('config')..'/lua/lsp/servers')) do
+  server = server:sub(0, -5) -- strip lua extension
   local opts = { on_attach = on_attach, capabilities = capabilities }
 
-  local req, tbl = pcall(require, 'lsp.servers.'..servers[i])
+  local req, tbl = pcall(require, 'lsp.servers.'..server)
   if req then opts = v.tbl_deep_extend('force', opts, tbl) end
-  lsp[servers[i]].setup(opts)
+  lsp[server].setup(opts)
 end
