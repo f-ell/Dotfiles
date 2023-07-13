9cdd6d74018579bb73f735b25a64e318a9fcc9fc
Nico Pareigis <nico@pareigis.de>
2023-07-13 10:23:02 +0200

    refactor: statuscolumn tweaks

--- source/.config/nvim/plugin/statuscolumn.lua
+++ source/.config/nvim/plugin/statuscolumn.lua
@@ -1,33 +1,29 @@
 local L = require('utils.lib')
-local d = require('lsp.ui.diagnostic')
-local signs, line
 
 local lnum = function()
-  return [[%{v:virtnum < 1 ? (v:relnum ? v:relnum : (v:lnum < 100 ? v:lnum.' ' : v:lnum)) : ''}]]
+  if vim.v.virtnum ~= 0 then return '' end
+  return vim.v.relnum == 0 and vim.v.lnum..' ' or vim.v.relnum
+end
+
+_G.statuscolumn_handler = function()
+  local pos = vim.fn.getmousepos()
+  vim.api.nvim_win_set_cursor(0, { pos.line, 0 })
+
+  local signs = vim.fn.sign_getplaced(vim.fn.bufnr(),
+    { group = '*', lnum = pos.line })[1].signs
+
+  for _, sign in pairs(signs) do
+    if (vim.startswith(sign.group, 'vim.diagnostic.vim.lsp.')) then
+      require('lsp.ui').dgn.get_line()
+      break
+    end
+  end
 end
 
 local statuscolumn = function()
   return table.concat({
-    '%=', lnum(), ' %@v:lua.statuscolumn_diagnostic@%s%X'
+    '%=', lnum(), ' %@v:lua.statuscolumn_handler@%s%X'
   })
-end
-
-_G.statuscolumn_diagnostic = function()
-  local pos = vim.fn.getmousepos()
-  vim.api.nvim_win_set_cursor(0, { pos.line, 0 })
-
-  if (line ~= pos.line) then
-    line = pos.line
-    signs = vim.fn.sign_getplaced(vim.fn.bufname(),
-      { group = '*', lnum = pos.line })[1].signs
-  end
-
-  for _, sign in pairs(signs) do
-    if (vim.startswith(sign.group, 'vim.diagnostic.vim.lsp.')) then
-      d.get_line()
-      break
-    end
-  end
 end
 
 _G.statuscolumn = statuscolumn
