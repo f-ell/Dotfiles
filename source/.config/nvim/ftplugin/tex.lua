7a0b58e3ecaa3b743d6a858953386fa214532da9
Nico Pareigis <nico@pareigis.de>
2023-07-27 20:52:42 +0200

    fix(e2dd177): check modified pre write

--- source/.config/nvim/ftplugin/tex.lua
+++ source/.config/nvim/ftplugin/tex.lua
@@ -1,10 +1,18 @@
-vim.api.nvim_create_autocmd('BufWritePost', {
+vim.api.nvim_create_autocmd('BufWritePre', {
   buffer = vim.api.nvim_get_current_buf(),
   callback = function()
-    if not vim.bo.modified then return end
-    -- NOTE: inefficient but requierd with delayed Vimtex load
-    if vim.g.loaded_vimtex and vim.g.vimtex_compiler_method == 'tectonic' then
-      vim.cmd('silent VimtexCompileSS');
+    if not (
+      vim.bo.modified
+      and vim.g.loaded_vimtex
+      and vim.g.vimtex_compiler_method == 'tectonic'
+      ) then
+      return
     end
+
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      once = true,
+      buffer = vim.api.nvim_get_current_buf(),
+      callback = function() vim.cmd('silent VimtexCompileSS') end
+    })
   end
 })
