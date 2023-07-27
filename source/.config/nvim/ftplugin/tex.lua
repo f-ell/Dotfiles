e2dd177c8fe05b221af55b753c95fc75f917b2f5
Nico Pareigis <nico@pareigis.de>
2023-07-27 20:47:05 +0200

    perf: early return if buffer not modified

--- source/.config/nvim/ftplugin/tex.lua
+++ source/.config/nvim/ftplugin/tex.lua
@@ -1,6 +1,7 @@
 vim.api.nvim_create_autocmd('BufWritePost', {
   buffer = vim.api.nvim_get_current_buf(),
   callback = function()
+    if not vim.bo.modified then return end
     -- NOTE: inefficient but requierd with delayed Vimtex load
     if vim.g.loaded_vimtex and vim.g.vimtex_compiler_method == 'tectonic' then
       vim.cmd('silent VimtexCompileSS');
