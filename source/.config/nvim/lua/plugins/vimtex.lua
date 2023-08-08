38295cd8199afb4dcc4b9c3583dda612155d87c3
Nico Pareigis <nico@pareigis.de>
2023-08-08 21:10:56 +0200

    chore(vimtex): configuration changes

--- source/.config/nvim/lua/plugins/vimtex.lua
+++ source/.config/nvim/lua/plugins/vimtex.lua
@@ -6,6 +6,7 @@ return {
     { '<leader>vl', '<CMD>VimtexClean<CR>' },
     { '<leader>vc', '<CMD>VimtexCompileSS<CR>' },
     { '<leader>vs', '<CMD>VimtexStatus!<CR>' },
+    { '<leader>vt', '<CMD>VimtexTocToggle<CR>' },
     { '<leader>vv', '<CMD>VimtexView<CR>' }
   },
   config = function()
@@ -13,5 +14,14 @@ return {
     vim.g('vimtex_view_method', 'zathura')
     vim.g('vimtex_compiler_method', 'tectonic')
     vim.g('vimtex_view_forward_search_on_start', 0)
+    vim.g('vimtex_toc_show_preable', 0)
+    vim.g('vimtex_toc_config', {
+      name = 'Table of Contents',
+      indent_levels = 1,
+      layer_status = { include = 0 },
+      show_help = 0,
+      split_pos = 'vert rightbelow',
+      split_width = 38
+    })
   end
 }
