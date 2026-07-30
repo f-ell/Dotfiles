f0ace5110b9592fe43d1e3703d76f3ecac36b4bc
Nico Pareigis <nico@pareigis.de>
2026-07-30 17:55:53 +0200

    feat(hypr): un-monocle on penultimate window move

--- src/hypr/dot-config/hypr/layout.lua
+++ src/hypr/dot-config/hypr/layout.lua
@@ -1,18 +1,24 @@
-hl.on('window.close', function()
-  local ws = hl.get_active_special_workspace() or hl.get_active_workspace()
-  if ws == nil then
-    hl.notification.create({
-      text = 'Invalid workspace.',
-      timeout = 3000,
-    })
-    return
-  end
+---@param count integer
+local function unmonocle_if_count(count)
+  return function()
+    local ws = hl.get_active_special_workspace() or hl.get_active_workspace()
+    if ws == nil then
+      hl.notification.create({
+        text = 'Invalid workspace.',
+        timeout = 3000,
+      })
+      return
+    end
 
-  local count = #ws:get_windows() - 1
-  if count == 1 and ws.tiled_layout == 'monocle' then
-    hl.workspace_rule({
-      workspace = tostring(ws.id),
-      layout = LAYOUT,
-    })
+    if #ws:get_windows() == count and ws.tiled_layout == 'monocle' then
+      hl.workspace_rule({
+        workspace = tostring(ws.id),
+        layout = LAYOUT,
+      })
+    end
   end
-end)
+end
+
+hl.on('window.close', unmonocle_if_count(1))
+hl.on('window.close', unmonocle_if_count(2))
+hl.on('window.move_to_workspace', unmonocle_if_count(1))
