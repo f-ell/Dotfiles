ee5379d7dcb33e8e61ae52984c81d50368331837
Nico Pareigis <nico@pareigis.de>
2026-08-05 15:46:11 +0200

    feat: layer fades

--- src/hypr/dot-config/hypr/animations.lua
+++ src/hypr/dot-config/hypr/animations.lua
@@ -15,6 +15,21 @@ local animations = {
   },
   { leaf = 'fade', enabled = true, speed = 0.6, bezier = 'default' },
   { leaf = 'border', enabled = true, speed = 4, bezier = 'default' },
+
+  {
+    leaf = 'fadeLayers',
+    enabled = true,
+    speed = 1.5,
+    bezier = 'default',
+  },
+  -- Affects layer resizes (e.g. for Rofi); has no effect on initial layer creation.
+  {
+    leaf = 'layersIn',
+    enabled = true,
+    speed = 1,
+    bezier = 'default',
+    style = 'fade',
+  },
 }
 
 for _, a in pairs(animations) do
